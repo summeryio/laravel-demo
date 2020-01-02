c428c0f443ee35e1e9fb1e186c1e3b9b599e19e1
summeryio <1005526074@qq.com>
2020-01-02 11:38:13 +0800

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.default')
+
+@section('title', '关于我')
+
+@section('content')
+    <h1>about</h1>
+@stop
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,8 @@
+
+@extends('layouts.default')
+
+@section('title', '帮助页')
+
+@section('content')
+    <h1>help</h1>
+@stop
