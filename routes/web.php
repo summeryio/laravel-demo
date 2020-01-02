ae84b3f3f70ee1da71d6db2ae8901faec2118282
summeryio <1005526074@qq.com>
2020-01-02 13:29:54 +0800

    路由别名的两种方式

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,11 @@
 
 
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', [
+    'as' => 'help',
+    'uses' => 'StaticPagesController@help'
+]);
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+
