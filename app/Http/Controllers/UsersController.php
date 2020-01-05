0d0e97a3f48e34c1a5e56d99fb146ff0cd3265ea
summeryio <summeryio@summeryiodeiMac.local>
2020-01-05 11:51:13 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,14 @@ class UsersController extends Controller
         // 将用户对象 $user 通过 compact 方法转化为一个关联数组，并作为第二个参数传递给 view 方法，将数据与视图进行绑定。
         return view('users.show', compact('user'));
     }
+
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required | max:50',
+            'email' => 'required | email | unique:users | max:255',
+            'password' => 'required | confirmed | min:2'
+        ]);
+        
+        return;
+    }
 }
