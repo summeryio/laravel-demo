414e1af9427a2c5662527dd56c412a75f2d4671f
summeryio <1005526074@qq.com>
2020-01-06 15:20:29 +0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -37071,8 +37071,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/summeryio/Code/laravel-demo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/summeryio/Code/laravel-demo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/a123/Code/laravel-demo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/a123/Code/laravel-demo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
