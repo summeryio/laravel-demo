2fd0b2913e97b395d9330cb5a3f43c1fe021d51c
summeryio <1005526074@qq.com>
2020-01-09 14:39:13 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,4 +73,32 @@ class User extends Authenticatable
         return $this->statuses()
             ->orderBy('created_at', 'desc');
     }
+
+    // 获取粉丝关系列表
+    public function followers() {
+        return $this->belongsToMany(User::class, 'follwers', 'user_id', 'follower_id');
+    }
+    // 获取用户关注人列表
+    public function followings() {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids) {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids, false);
+    }
+    public function unfollow($user_ids) {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids, false);
+    }
+
+    public function isFollowing($user_id) {
+        return $this->followings->constains($user_id);
+    }
 }
